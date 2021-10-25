93f08f2f3617fc6eca8181198b4fe7b36c5baf1f
wjddmswn55 <91711511+wjddmswn55@users.noreply.github.com>
2021-10-26 01:06:24 +0900

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,42 @@
+//////////////////// scroll ///////////////////////////
+$(function(){
+    var menu = $('#header_in > ul > li');
+    var contents = $('#scroll > *');
+    
+    menu.click(function(event){
+       event.preventDefault();
+       
+       var tg = $(this);
+       var i = tg.index();
+       
+       var section = contents.eq(i);
+       var tt = section.offset().top;
+       
+       $('html, body').stop().animate({scrollTop:tt});
+    });
+    
+     $(window).scroll(function(){
+       var sct = $(window).scrollTop();
+      
+       contents.each(function(){
+         var tg = $(this);
+         var i = tg.index();
+          if(tg.offset().top <= sct){
+              menu.removeClass('on');
+              menu.eq(i).addClass('on');
+          }
+      });
+    });
+});
+
+//////////////////////////// project1 /////////////////////////////////
+$(function(){
+  $('.project1_2').hide();
+  
+  $('.project1').click(function(){
+         $('.project1_2').hide();
+         $(this).next('.project1_2').show();
+
+  });
+  });
+
